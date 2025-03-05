3a2536aac7cd9871f1733b382f66c0450c9064b2
Nicholas Novak <34256932+NickyBoy89@users.noreply.github.com>
2025-03-05 01:09:34 -0800

    feat: Added a prototype of the menu editor

---
+++ src/routes/(app)/edit-menu/[menu_id]/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    import MenuRenderer from "$lib/components/menuRenderer.svelte";
+    import MenuItemEditor from "$lib/components/menuItemEditor.svelte";
+    import UiButton from "$lib/components/uiButton.svelte";
+
+    let { data } = $props();
+</script>
+
+<div class="grid grid-cols-2">
+    <div class="menu-editor p-6">
+        <div class="menu-editors grid gap-4">
+            {#each data.menuItems as menuItem}
+                <MenuItemEditor name={menuItem.name} description={menuItem.description} />
+            {/each}
+        </div>
+        <div class="flex justify-between mt-4">
+            <UiButton text="New" action={() => {alert("New menu item")}} color="#fb4934"/>
+            <UiButton text="Save" action={() => {alert("Save menu items")}} color="#98971a"/>
+        </div>
+    </div>
+
+    <section class="menu-preview">
+        <MenuRenderer menuName={data.menu.name} menuDate={new Date(data.menu.date)}/>
+    </section>
+</div>
---
+++ src/routes/(app)/edit-menu/[menu_id]/+page.ts
@@ -0,0 +1,14 @@
+import type { PageLoad } from './$types';
+
+export const load: PageLoad = async ({ fetch, params }) => {
+    const menuData = await fetch(`http://localhost:9031/api/menu/${params.menu_id}`);
+    const menu = await menuData.json();
+
+    const menuItemData = await fetch(`http://localhost:9031/api/menu/${params.menu_id}/items`)
+    const items = await menuItemData.json()
+
+    return {
+        menu: menu,
+        menuItems: items
+    }
+}
