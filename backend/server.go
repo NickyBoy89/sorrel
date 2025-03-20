054dd656373a74f64a7c15ac81c4274aa960ddd8
Nicholas Novak <34256932+NickyBoy89@users.noreply.github.com>
2025-03-20 08:56:47 -0700

    feat: Added a lot more data for modeling

--- backend/server.go
+++ backend/server.go
@@ -28,19 +28,16 @@ type MenuItem struct {
 }
 
 func initDb(db *sql.DB) error {
-	query := `CREATE TABLE IF NOT EXISTS menus(
+	if _, err := db.Exec(`CREATE TABLE IF NOT EXISTS menus(
 		id INTEGER PRIMARY KEY AUTOINCREMENT,
 		name TEXT NOT NULL,
 		date TIMESTAMP
 	);
-	`
-
-	_, err := db.Exec(query)
-	if err != nil {
+	`); err != nil {
 		return err
 	}
 
-	query = `CREATE TABLE IF NOT EXISTS items(
+	if _, err := db.Exec(`CREATE TABLE IF NOT EXISTS items(
 		id INTEGER PRIMARY KEY AUTOINCREMENT,
 		menu_id INTEGER NOT NULL,
 		name TEXT NOT NULL,
@@ -48,10 +45,66 @@ func initDb(db *sql.DB) error {
 		menu_section TEXT,
 		FOREIGN KEY(menu_id) REFERENCES menus(id)
 	);
-	`
+	`); err != nil {
+		return err
+	}
 
-	_, err = db.Exec(query)
-	if err != nil {
+	if _, err := db.Exec(`CREATE TABLE IF NOT EXISTS users(
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		display_name TEXT NOT NULL
+	);
+	`); err != nil {
+		return err
+	}
+
+	if _, err := db.Exec(`CREATE TABLE IF NOT EXISTS notification_subscriptions(
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		user_id INTEGER NOT NULL,
+		endpoint TEXT NOT NULL,
+		keys_auto TEXT NOT NULL,
+		keys_p256dh TEXT NOT NULL,
+		FOREIGN KEY(user_id) REFERENCES users(id)
+	);
+	`); err != nil {
+		return err
+	}
+
+	if _, err := db.Exec(`CREATE TABLE IF NOT EXISTS shared_menus(
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		menu_id INTEGER NOT NULL,
+		user_id INTEGER NOT NULL,
+		FOREIGN KEY(menu_id) REFERENCES menus(id),
+		FOREIGN KEY(user_id) REFERENCES users(id)
+	);
+	`); err != nil {
+		return err
+	}
+
+	if _, err := db.Exec(`CREATE TABLE IF NOT EXISTS shopping_lists(
+		id INTEGER PRIMARY KEY AUTOINCREMENT
+	);
+	`); err != nil {
+		return err
+	}
+
+	if _, err := db.Exec(`CREATE TABLE IF NOT EXISTS shared_lists(
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		shopping_list_id INTEGER NOT NULL,
+		user_id INTEGER NOT NULL,
+		FOREIGN KEY(shopping_list_id) REFERENCES shopping_lists(id),
+		FOREIGN KEY(user_id) REFERENCES users(id)
+	);
+	`); err != nil {
+		return err
+	}
+
+	if _, err := db.Exec(`CREATE TABLE IF NOT EXISTS grocery_item(
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		name TEXT NOT NULL,
+		category TEXT,
+		quantity TEXT NOT NULL
+	);
+	`); err != nil {
 		return err
 	}
 
