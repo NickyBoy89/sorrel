4c2fba130be26f7ea767756816bd8503ba43d1f2
Nicholas Novak <34256932+NickyBoy89@users.noreply.github.com>
2025-03-20 23:38:24 -0700

    change: Cleaned up a http request handler

--- src/routes/(app)/+page.ts
+++ src/routes/(app)/+page.ts
@@ -2,8 +2,11 @@ import type { PageLoad } from './$types';
 import { backendRootURL } from '../../constants';
 
 export const load: PageLoad = async ({ fetch }) => {
-    const menusData = await fetch(`${backendRootURL}/api/menu/list`);
-    const menus = await menusData.json();
+    const menus = await fetch(`${backendRootURL}/api/menu/list`)
+    .then((resp) => resp.json()).catch((error) => {
+        console.error(error);
+        return {};
+    });
 
     return {
         menus: menus,
