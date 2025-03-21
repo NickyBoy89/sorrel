4fcae1d073f523e76d9d8d00e995682ce13092f5
Nicholas Novak <34256932+NickyBoy89@users.noreply.github.com>
2025-03-21 02:25:16 -0700

    fix: Removed server-side fetch and moved it to the client

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,13 +1,22 @@
 <script lang="ts">;
     import MenuEditor from "$lib/components/menuEditor.svelte";
+    import { backendRootURL } from "../../constants.js";
     let { data } = $props();
+
+    const menuList = fetch(`${backendRootURL}/api/menu/list`).then((resp) => resp.json())
 </script>
 
 <h1 class="text-4xl text-center my-4">Shared With You</h1>
 <div class="flex flex-col my-4">
-    {#each data.menus as menu}
+    {#await menuList}
+        Loading...
+    {:then menus}
+    {#each menus as menu}
         <MenuEditor menuName={menu.name} menuDate={new Date(menu.date)} menuId={menu.id} canEdit={false}/>
     {/each}
+    {:catch error}
+        There was an error fetching the menus: {error}
+    {/await}
 </div>
 
 <h1 class="text-4xl text-center my-4">Shopping Lists</h1>
