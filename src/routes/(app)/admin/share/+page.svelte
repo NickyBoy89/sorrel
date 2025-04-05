c5875421e4fd296058b3572d05253c055e99d86e
Nicholas Novak <34256932+NickyBoy89@users.noreply.github.com>
2025-04-05 00:01:31 -0700

    feat: More progress on share page

--- src/routes/(app)/admin/share/+page.svelte
+++ src/routes/(app)/admin/share/+page.svelte
@@ -1,19 +1,49 @@
 <script lang="ts">
     import UiButton from "$lib/components/uiButton.svelte";
+    import { onMount } from "svelte";
     import { backendRootURL } from "../../../../constants";
+    import Fa from 'svelte-fa'
+    import { faSpinner, type IconDefinition } from "@fortawesome/free-solid-svg-icons";
 
+    type User = {
+        id: number,
+        display_name: string,
+    };
+
+    let users = $state([] as Array<User>);
+
+    let icons = $state([] as Array<null | IconDefinition>);
+    let selected = [];
+
+    onMount(() => {
+        fetch(`${backendRootURL}/api/users`)
+            .then((resp) => resp.json())
+            .then((resp) => users = resp)
+            .catch((error) => console.error(error));
+
+        selected = Array(users.length).fill(false);
+        icons = Array(users.length).fill(null);
+    });
+
+    const handleUserChecked = (event: Event & { currentTarget: EventTarget & HTMLInputElement}) => {
+        const ind = event?.currentTarget?.dataset.index;
+        if (ind == null) return;
+        selected[Number.parseInt(ind)] = event?.currentTarget?.checked;
+    }
+
+    const handleSendNotifications = () => {
+        users.forEach((userId, ind) => {
+            icons[ind] = faSpinner;
+        });
+    };
 </script>
 
-{#await fetch(`${backendRootURL}/api/users`).then((resp) => resp.json())}
-Loading...
-{:then users}
-<ol>
-{#each users as user}
-<li>Id: {user.id}, Name: {user.display_name}</li>
-{/each}
-</ol>
-{:catch error}
-There was an error fetching the users: {error}
-{/await}
-
-<UiButton text="Share" action={() => {}}/>
+<div class="text-black dark:text-white">
+    <ol>
+    {#each users as user, ind}
+    <li>Id: {user.id}, Name: {user.display_name}<input type="checkbox" data-index={ind} onchange={handleUserChecked}>{#if icons[ind] != null}<Fa icon={icons[ind]} />{/if}</li>
+    {/each}
+    </ol>
+    
+    <UiButton text="Share" action={() => {}}/>
+</div>
--- src/routes/(app)/admin/users/+page.svelte
+++ src/routes/(app)/admin/users/+page.svelte
@@ -3,14 +3,16 @@
 
 </script>
 
-{#await fetch(`${backendRootURL}/api/users`).then((resp) => resp.json())}
-Loading...
-{:then users}
-<ol>
-{#each users as user}
-<li>Id: {user.id}, Name: {user.display_name}</li>
-{/each}
-</ol>
-{:catch error}
-There was an error fetching the users: {error}
-{/await}
+<div class="text-black dark:text-white">
+    {#await fetch(`${backendRootURL}/api/users`).then((resp) => resp.json())}
+    Loading...
+    {:then users}
+    <ol>
+    {#each users as user}
+    <li>Id: {user.id}, Name: {user.display_name}</li>
+    {/each}
+    </ol>
+    {:catch error}
+    There was an error fetching the users: {error}
+    {/await}
+</div>
