6a94c5e795bfafcf26b5c063b41ea71a2ed869ed
Nicholas Novak <34256932+NickyBoy89@users.noreply.github.com>
2025-05-09 00:44:15 -0700

    change: Separated out the auth handler

---
+++ src/lib/auth.ts
@@ -0,0 +1,31 @@
+import { keycloakClientId, keycloakRealm, keycloakUrl } from '../constants';
+import Keycloak, { type KeycloakInitOptions } from 'keycloak-js';
+import { bearerToken } from '../routes/(app)/stores';
+
+let instance = {
+    url: keycloakUrl,
+    realm: keycloakRealm,
+    clientId: keycloakClientId
+};
+
+const initOptions: KeycloakInitOptions = {
+    onLoad: "check-sso",
+};
+
+const keycloak = new Keycloak(instance);
+
+export const initKeycloak = () => {
+    if (!keycloak.authenticated) {
+        keycloak.init(initOptions)
+        .then((auth) => {
+            if (auth) {
+                console.log("Auth successful!");
+            }
+            bearerToken.set(keycloak.token);		
+        })
+        .catch((error) => { 
+            console.error("Auth not successful")
+            console.error(error)
+        });
+    }
+};
