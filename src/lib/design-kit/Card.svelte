aa91a15244aa00a60021f133c8175aba1c126206
Nicholas Novak <34256932+NickyBoy89@users.noreply.github.com>
2026-04-30 22:06:24 -0700

    feat: Added card component

---
+++ src/lib/design-kit/Card.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import type { Snippet } from "svelte";
+
+  type CardProps = {
+    children: Snippet;
+  };
+
+  let { children }: CardProps = $props();
+</script>
+
+<div
+  class="flex w-32 h-24 rounded-lg bg-neutral-700 items-center justify-center"
+>
+  {@render children()}
+</div>
